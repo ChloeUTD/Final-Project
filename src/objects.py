0d2f874fe05314a51c8af72249d7db062c1f03f3
Chloe <cnh220001@utdallas.edu>
2023-11-17 15:45:07 -0600

    web and lumberjack class added

--- src/objects.py
+++ src/objects.py
@@ -5,3 +5,15 @@ class Bush(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load(img)
         self.rect = self.image.get_rect()
+
+class Web(pygame.sprite.Sprite):
+    def __init__(self, img):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(img)
+        self.rect = self.image.get_rect()        
+
+class Lumberjack(pygame.sprite.Sprite):
+    def __init__(self, img):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(img)
+        self.rect = self.image.get_rect()        

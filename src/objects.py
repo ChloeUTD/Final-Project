ca7ef34f68bdd983bb9e6d25fa6b782689fbf8c9
Chloe <cnh220001@utdallas.edu>
2023-11-22 12:35:52 -0600

    update method added to all objects

--- src/objects.py
+++ src/objects.py
@@ -1,19 +1,37 @@
 import pygame
 
 class Bush(pygame.sprite.Sprite):
-    def __init__(self, img):
+    def __init__(self, img, pos=(1000,544)):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load(img)
         self.rect = self.image.get_rect()
+        self.pos = pos
+        
+    def update(self):
+        x, y = self.pos
+        x -= 10
+        self.pos = (x, y)
 
 class Web(pygame.sprite.Sprite):
-    def __init__(self, img):
+    def __init__(self, img, pos=(1000, 0)):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load(img)
-        self.rect = self.image.get_rect()        
+        self.rect = self.image.get_rect()
+        self.pos = pos
+
+    def update(self):
+        x, y = self.pos
+        x -= 10
+        self.pos = (x, y)
 
 class Lumberjack(pygame.sprite.Sprite):
-    def __init__(self, img):
+    def __init__(self, img, pos=(1000, 0)):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load(img)
-        self.rect = self.image.get_rect()        
+        self.rect = self.image.get_rect()
+        self.pos = pos
+
+    def update(self):
+        x, y = self.pos
+        x -= 10
+        self.pos = (x, y)
